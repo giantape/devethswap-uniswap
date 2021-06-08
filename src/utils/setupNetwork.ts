93057f9ce74264302453d6855cc73b5f1d9a2807
g14n4p3 <g14n4p3@g14n4p3s-MBP.fritz.box>
2021-06-08 22:40:23 +0200

    first commit

---
+++ src/utils/setupNetwork.ts
@@ -0,0 +1,31 @@
+export const setupNetwork = async () => {
+  const provider = (window as Window).ethereum
+  if (provider && provider.request) {
+    const chainId = 787
+    try {
+      await provider.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: chainId,
+            chainName: 'dth',
+            nativeCurrency: {
+              name: 'dth',
+              symbol: 'dth',
+              decimals: 18
+            },
+            rpcUrls: [process.env.REACT_APP_NETWORK_URL],
+            blockExplorerUrls: ['https://explore.deveth.org/']
+          }
+        ]
+      })
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  } else {
+    console.error("Can't setup the deveth network on metamask because window.ethereum is undefined")
+    return false
+  }
+}
